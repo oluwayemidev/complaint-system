0780b34e94b786d4c25095d537eb770b54a368bd
Sunday Olanisimi <lordryefox@gmail.com>
2024-04-06 00:24:26 +0100

    first commit

--- api/index.ts
+++ api/index.ts
@@ -1,35 +1,8 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const mongoose = require('mongoose')
-const userRoutes = require('../routes/userRoutes')
-const { requireAuth, checkUser } = require('../middleware/authMiddleware')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const app = express();
 
-// Middleware
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cookieParser())
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// View Engine
-app.set('view engine', 'ejs')
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// Database Connection
-const dbURL = "mongodb+srv://admin:test123@node-tuts.fytvyoz.mongodb.net/school-portal"
-mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(port))
-    .catch((err) => console.log(err))
-
-// Routes
-app.get('*', checkUser)
-app.get('/', (req, res) => {
-    res.render('home', { title: '' })
-})
-app.get('/admin', (req, res) => {
-    res.render('admin', { title: 'Admin' })
-})
-app.use(userRoutes)
-app.get('/chat', requireAuth, (req, res) => {
-    res.render('chat', { title: 'chat' })
-})
+module.exports = app;
